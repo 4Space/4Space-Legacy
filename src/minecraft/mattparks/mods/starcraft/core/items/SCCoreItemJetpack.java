d8c61ccabb24b0c973e036e71f44fa0a0ca62a54
Matt Albrecht <mattparks5855@gmail.com>
2013-11-02 18:51:07 -0500

    Redo jetpack texture finder

--- src/minecraft/mattparks/mods/starcraft/core/items/SCCoreItemJetpack.java
+++ src/minecraft/mattparks/mods/starcraft/core/items/SCCoreItemJetpack.java
@@ -1,10 +1,16 @@
 package mattparks.mods.starcraft.core.items;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import mattparks.mods.starcraft.core.SCCoreConfigManager;
 import mattparks.mods.starcraft.core.StarcraftCore;
+import micdoodle8.mods.galacticraft.core.client.ClientProxyCore;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumArmorMaterial;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
@@ -12,16 +18,52 @@ import net.minecraft.world.World;
 
 public class SCCoreItemJetpack extends ItemArmor
 {
+    	private final EnumArmorMaterial material;
         public boolean active;
 
         public SCCoreItemJetpack(int par1, EnumArmorMaterial par2EnumArmorMaterial, int par3, int par4, boolean breathable)
         {
             super(par1, par2EnumArmorMaterial, par3, par4);
+            	this.material = par2EnumArmorMaterial;
                 this.setMaxStackSize(1);
                 this.setMaxDamage(256);
-                this.setCreativeTab(StarcraftCore.starcraftCoreTab);
+        }
+        
+        @Override
+        public CreativeTabs getCreativeTab()
+        {
+            return StarcraftCore.starcraftCoreTab;
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public EnumRarity getRarity(ItemStack par1ItemStack)
+        {
+            return ClientProxyCore.galacticraftItem;
+        }
+        
+        @Override
+        @SideOnly(Side.CLIENT)
+        public void registerIcons(IconRegister par1IconRegister)
+        {
+            this.itemIcon = par1IconRegister.registerIcon(this.getUnlocalizedName().replace("item.", "starcraftcore:"));
         }
 
+        @Override
+        public String getArmorTexture(ItemStack stack, Entity entity, int slot, int layer)
+        {
+            if (this.material == SCCoreItems.ARMORJETPACK)
+            {
+                if (stack.getItem().itemID == SCCoreItems.jetpack.itemID)
+                {
+                    return "textures/model/armor/jetpack.png";
+
+                }
+            }
+
+            return null;
+        }
+        
         @Override
         public void onArmorTickUpdate(World world, EntityPlayer player, ItemStack itemstack)
         {
