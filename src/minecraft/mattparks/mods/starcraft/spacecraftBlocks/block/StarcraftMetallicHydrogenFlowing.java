5362fd5a4fbee02cc62d689a85c6a389d389a3bd
danbka33 <danbka33rus@gmail.com>
2013-11-02 18:41:03 +0400

    Fix compiled error

---
+++ src/minecraft/mattparks/mods/starcraft/spacecraftBlocks/block/StarcraftMetallicHydrogenFlowing.java
@@ -0,0 +1,17 @@
+package mattparks.mods.starcraft.spacecraftBlocks.block;
+
+import net.minecraft.block.BlockFlowing;
+import net.minecraft.block.material.Material;
+
+public class StarcraftMetallicHydrogenFlowing extends BlockFlowing
+{
+	public StarcraftMetallicHydrogenFlowing(int par1)
+	{
+		super(par1, Material.lava);
+	}
+
+	{
+		this.blockHardness = 100.0F;
+		this.setLightOpacity(1);
+	}
+}
