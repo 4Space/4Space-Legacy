c4d52de783abee50c565b23bf6b5d477052873ce
Matthew Albrecht <mattparks5855@gmail.com>
2013-11-28 19:54:52 -0600

    Create mcmod.info file

--- src/minecraft/mattparks/mods/starcraft/core/StarcraftCore.java
+++ src/minecraft/mattparks/mods/starcraft/core/StarcraftCore.java
@@ -49,7 +49,7 @@ public class StarcraftCore
 
     public static final int LOCALMAJVERSION = 0;
     public static final int LOCALMINVERSION = 0;
-    public static final int LOCALBUILDVERSION = 1;
+    public static final int LOCALBUILDVERSION = 2;
     public static int remoteMajVer;
     public static int remoteMinVer;
     public static int remoteBuildVer;
